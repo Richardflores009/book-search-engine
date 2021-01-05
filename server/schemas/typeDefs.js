b7018d4af2e8619491fa5d90599682c4bb684149
Richy Rich <richardflores009@gmail.com>
2021-01-04 17:41:37 -0700

    added package stuff

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,53 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  input books {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input:books): User
+    removeBook(bookId: String!): User
+  }
+  
+
+  
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
